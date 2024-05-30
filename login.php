1d152cadb10dac37394a832bea342bfb8052295d
M.Nabil Amin <168261527+9ur50n@users.noreply.github.com>
2024-05-30 23:47:23 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,7 +33,8 @@
         </h1>
         
         <!-- Login form -->
-        <form id="loginForm" class="bg-white bg-opacity-70 dark:bg-gray-800 dark:bg-opacity-70 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md">
+        <form id="loginForm" class="bg-white bg-opacity-70 dark:bg-gray-800 dark:bg-opacity-70 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md" action="login.php">
+        <?php include('errors.php'); ?>
           <div class="mb-4">
             <label class="block text-gray-700 dark:text-white text-sm font-bold mb-2" for="username">Username</label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" name="username" required>
@@ -43,7 +45,7 @@
           </div>
          
           <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Sign In</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="login_user">Sign In</button>
             <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">Forgot Password?</a>
           </div>
         </form>
@@ -57,7 +59,8 @@
   <script>
     // Function to handle form submission and redirection
     document.getElementById("loginForm").addEventListener("submit", function(event) {
-      event.preventDefault(); // Prevent default form submission
+      event.preventDefault(); 
+      // Prevent default form submission
       
       // Get username and password from form
       var username = document.getElementById("username").value;
